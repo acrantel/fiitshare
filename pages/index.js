c0a4f4f5e652241a1a255d096a6ab52aec44cfe0
Serena Li <serena.li.usa@gmail.com>
2020-07-12 10:03:48 -0700

    initialize db

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import Header from '../components/header.js';
-import Sidebar from '../components/sidebar.js';
+import Sidebar from '../components/dashboard/sidebar.js';
 
 
 class Home extends React.Component {
@@ -8,10 +8,14 @@ class Home extends React.Component {
         return <div>
             <Header />
             <div>
-                <Sidebar imageSrc="/images/testuser.jpeg" title="User user" 
+                <Sidebar imageSrc="/images/testuser.jpeg" title="John Doe" 
                 workoutTime="10 hours 15 minutes" calories="2000" numWorkouts={20}/>
+                
             </div>
         </div>
+    }
+    componentDidMount() {
+        document.body.style.backgroundColor = '#f7f7fa';
     }
 }
 export default Home
