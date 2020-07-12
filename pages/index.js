d516e7a8e279e42a6447873ffa72955884e9137e
Serena Li <serena.li.usa@gmail.com>
2020-07-12 11:41:53 -0700

    front page structure

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,20 @@
 import React, { Component } from 'react';
 import Header from '../components/header.js';
 import Sidebar from '../components/dashboard/sidebar.js';
-import Dashboard from '../components/dashboard/dashboard-center.js';
-import {userData, workoutData, exerciseData, groupData} from '../database/database.js';
-const USERID='user1';
+import Dashboard from '../components/dashboard/dashboard.js';
+import { userData, workoutData, exerciseData, groupData } from '../database/database.js';
+
+const USERID = 'user1';
 
 class Home extends React.Component {
     render() {
         return <div>
             <Header />
-            <div>
-                <Sidebar imageSrc={userData[USERID]['picture']} title={userData[USERID]['name']} 
-                workoutTime={userData[USERID]['time_spent']} calories={userData[USERID]['calories']} 
-                numWorkouts={userData[USERID]['completed_workouts']}/>
-                <Dashboard userId={USERID}/>
+            <div styles={{backgroundColor:'pink', display:'flex', flexDirection:'row', marginLeft: 'auto'}}>
+                <Sidebar imageSrc={userData[USERID]['picture']} title={userData[USERID]['name']}
+                    workoutTime={userData[USERID]['time_spent']} calories={userData[USERID]['calories']}
+                    numWorkouts={userData[USERID]['completed_workouts']} />
+                <Dashboard userId={USERID} />
             </div>
         </div>
     }
