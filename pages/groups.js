39389349c0814edc34a971ec5bf083d4d1e8a86e
pinkbluesky <pinkbluesky@users.noreply.github.com>
2020-07-12 13:57:40 -0700

    add filter and Join by id button to Find Groups

--- pages/groups.js
+++ pages/groups.js
@@ -6,7 +6,22 @@ import { MdSearch } from 'react-icons/md'
 
 const USERID = 'user1';
 class Groups extends React.Component {
-   
+
+    searchGroups()
+    {
+
+    }
+    
+    // onchange handler for the group name search text field
+    nameChangeHandler = (event) => {
+        this.setState({ nameSearch: event.target.value });
+    }
+
+    // onchange handler for the group name search text field
+    levelChangeHandler = (event) => {
+        this.setState({ nameSearch: event.target.value });
+    }
+
     render() {
         var myGroupsRender = [];
         // for each of the user's groups
@@ -14,24 +29,33 @@ class Groups extends React.Component {
         {
             myGroupsRender.push(<GroupCard groupID={userGroup} isYours={true} />);
         }
-        return <div className='groups'>
-            <Header />
+        return  <div><Header /> 
+        <div className='groups'>
+           
             <div className='my-groups-container'>
                 <h2 className='groups-title'>My Groups</h2>
 
                 <div className='my-groups-list'>
-                    {myGroupsRender}
+                        {myGroupsRender}
+                    </div>
                 </div>
-            </div>
-            <div className='find-groups-container'>
+                <div className='find-groups-container'>
                 <h2 className='groups-title'>Find Groups</h2>
-                <div className='find-groups-filter'>
-                <MdSearch className='searchIcon'/>
-            <input placeholder="Name" type="text"></input>
+                <div className='find-groups-line-2'>
+                    <div className='find-groups-filter'>
+                        <input onChange={this.nameChangeHandler} placeholder="Name" type="text"></input>
+                        <select>
+                            <option selected value="beginner">Beginner</option>
+                            <option value="intermediate">Intermediate</option>
+                            <option value="advanced">Advanced</option>
+                        </select>
+                        <button><MdSearch className='searchIcon' /></button>
+                    </div>
+                    <button className='join-group-id-button'>Join by ID</button>
+                </div>
                 </div>
             </div>
         </div>
-        
     }
 }
 export default Groups
