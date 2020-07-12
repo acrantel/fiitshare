ea4730db124628cb9503705ecd0e0105a84e92d9
Serena Li <serena.li.usa@gmail.com>
2020-07-12 10:34:11 -0700

    fix database

--- components/dashboard/dashboard-center.js
+++ components/dashboard/dashboard-center.js
@@ -1,14 +1,24 @@
 import React, { Component } from 'react';
+import styles from './dashboard-center.module.css';
+import {userData, workoutData, exerciseData, groupData} from '../../database/database.js';
+import WorkoutCard from '../cards/workout-card.js'
 
 class Dashboard extends React.Component {
     render () {
         return <div>
-            <Recent userId={this.props.userId}/>
-            <Feed/>
         </div>
     }
 }
 
-
+function Recent({userId}) {
+    return <div className={styles.recentSection}>
+        <h1 className={styles.sectionTitle}>Recent</h1>
+        <ul className={styles.sectionList}>
+            {userData[userId]['recent_workouts'].map(function(item) {
+                return <WorkoutCard workoutId={item}/>
+            })}
+        </ul>
+    </div>
+}
 
 export default Dashboard;
