a6859ca8dd676cc1d53e876e82df3c41713343d6
Serena Li <serena.li.usa@gmail.com>
2020-07-11 23:47:58 -0700

    some sidebar stuff

--- components/sidebar.js
+++ components/sidebar.js
@@ -1,8 +1,36 @@
 import React, { Component } from 'react';
-import Link from 'next/link'
-import styles from './search-bar.module.css'
-import { Search } from '@material-ui/icons';
+import Link from 'next/link';
+import styles from './sidebar.module.css';
+import { MdQueryBuilder } from 'react-icons/md';
+import { AiFillFire } from 'react-icons/ai';
+import { FaRegChartBar } from 'react-icons/fa';
 
-class Sidebar extends React.Component {
-    
+function Sidebar({ imageSrc, title, workoutTime, calories, numWorkouts }) {
+    return <div className={styles.sidebar}>
+            <img className={styles.image} src={imageSrc} alt={title} />
+        <h1 className={styles.title}>{title}</h1>
+        <div className={styles.bottom}>
+            <div className={styles.info}>
+                <MdQueryBuilder className={styles.icon} />
+                <p className={styles.label}>{workoutTime}</p>
+            </div>
+            <div className={styles.info}>
+                <AiFillFire className={styles.icon} />
+                <p className={styles.label}>{calories}</p>
+            </div>
+            <div className={styles.info}>
+                <FaRegChartBar className={styles.icon} />
+                <p className={styles.label}>{numWorkouts} workouts</p>
+            </div>
+        </div>
+    </div>
 }
+
+function SideInfo({ icon, text }) {
+    return <div>
+
+        <div className={styles.icon}>{icon}</div>
+    </div>
+}
+
+export default Sidebar;
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,17 @@
 import React, { Component } from 'react';
-import Header from '../components/header.js'
+import Header from '../components/header.js';
+import Sidebar from '../components/sidebar.js';
+
 
 class Home extends React.Component {
     render() {
-        return <Header/>
+        return <div>
+            <Header />
+            <div>
+                <Sidebar imageSrc="/images/testuser.jpeg" title="User user" 
+                workoutTime="10 hours 15 minutes" calories="2000" numWorkouts={20}/>
+            </div>
+        </div>
     }
 }
 export default Home
