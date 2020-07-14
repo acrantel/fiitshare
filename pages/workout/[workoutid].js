1a79f2578f5a394b9ae36f5dd892ab99ee32da94
SheepTester <ussa.say@gmail.com>
2020-07-14 15:27:41 -0700

    Fix getting workoutId from query and odd Date inconsistencies (I think)

--- pages/workout/[workoutid].js
+++ pages/workout/[workoutid].js
@@ -7,13 +7,6 @@ import {workoutData, exerciseData} from '../../database/database.js';
 import ExerciseCard from '../../components/cards/exercise-card.js';
 import { MdSkipPrevious, MdPlayArrow, MdPause, MdSkipNext } from 'react-icons/md'
 
-// TEMP: need to figure out dynamic routes getting query string
-const workoutID = 'wk1';
-
-const exerciseIDArr = workoutData[workoutID].exercises.exerciseId;
-const timeArr = workoutData[workoutID].exercises.time;
-const numSets = workoutData[workoutID].sets;
-
 class WorkoutVideo extends React.Component {
 
     constructor(props) {
@@ -24,30 +17,41 @@ class WorkoutVideo extends React.Component {
         this.playWorkout = this.playWorkout.bind(this);
         this.checkState = this.checkState.bind(this);
 
+        const workoutID = props.workoutId;
+        const timeArr = workoutData[workoutID].exercises.time;
         let today = new Date();
         today.setSeconds(today.getSeconds() + timeArr[0]);
         // set the state
-        this.state = { curIndex: 0, curSet: 1, timeToStop: today, paused: false, nowAndStopDiff: 0 };
+        this.state = {
+            curIndex: 0,
+            curSet: 1,
+            timeToStop: today,
+            paused: false,
+            nowAndStopDiff: 0,
+            exerciseIDArr: workoutData[workoutID].exercises.exerciseId,
+            timeArr,
+            numSets: workoutData[workoutID].sets
+        };
         setInterval(this.checkState, 1000); // check state every second
     }
 
     checkState() {
         //if (!(this.state.paused)) {
-            if (this.state.curIndex + 1 < exerciseIDArr.length) {
+            if (this.state.curIndex + 1 < this.state.exerciseIDArr.length) {
                 // if the exercise has ended
-                if (Date.now() >= this.state.timeToStop) {
+                if (new Date() >= this.state.timeToStop) {
                     // update with new exercise and new timetostop
                     let newTimeToStop = this.state.timeToStop;
-                    newTimeToStop.setSeconds(this.state.timeToStop.getSeconds() + timeArr[this.state.curIndex + 1]);
+                    newTimeToStop.setSeconds(this.state.timeToStop.getSeconds() + this.state.timeArr[this.state.curIndex + 1]);
                     this.setState({ curIndex: this.state.curIndex + 1, timeToStop: newTimeToStop });
                 }
             }
             else // when the one exercise array has finished
             {
                 // if there are still more sets to do
-                if (this.state.curSet + 1 <= numSets) {
+                if (this.state.curSet + 1 <= this.state.numSets) {
                     let newTimeToStop = this.state.timeToStop;
-                    newTimeToStop.setSeconds(this.state.timeToStop.getSeconds() + timeArr[0]);
+                    newTimeToStop.setSeconds(this.state.timeToStop.getSeconds() + this.state.timeArr[0]);
                     this.setState({ curIndex: 0, curSet: this.state.curSet + 1, timeToStop: newTimeToStop });
                 }
             }
@@ -58,20 +62,20 @@ class WorkoutVideo extends React.Component {
     skipNextExercise()
     {
         // check if it is the final exercise of the final set (workout is done)
-        if ((this.state.curIndex + 1) >= exerciseIDArr.length && this.state.curSet >= numSets)
+        if ((this.state.curIndex + 1) >= this.state.exerciseIDArr.length && this.state.curSet >= this.state.numSets)
         {
             // do nothing, workout is done
         }
         else {
             // check if its the last exercise of a set & we should move onto the next set
-            if ((this.state.curIndex + 1) >= exerciseIDArr.length) {
-                let tempTime = this.state.timeToStop;
-                tempTime.setSeconds(tempTime.getSeconds() + timeArr[0]);
+            if ((this.state.curIndex + 1) >= this.state.exerciseIDArr.length) {
+                let tempTime = new Date(this.state.timeToStop);
+                tempTime.setSeconds(tempTime.getSeconds() + this.state.timeArr[0]);
                 this.setState({curIndex: 0, curSet: this.state.curSet + 1, timeToStop: tempTime});
             }
             else { // otherwise just increment the current index
-                let tempTime = this.state.timeToStop;
-                tempTime.setSeconds(tempTime.getSeconds() + timeArr[this.state.curIndex + 1]);
+                let tempTime = new Date(this.state.timeToStop);
+                tempTime.setSeconds(tempTime.getSeconds() + this.state.timeArr[this.state.curIndex + 1]);
                 this.setState({ curIndex: this.state.curIndex + 1, timeToStop: tempTime });
             }
         }
@@ -81,15 +85,15 @@ class WorkoutVideo extends React.Component {
     pauseWorkout()
     {
         // record the difference (in seconds) between 'now' and timetostop
-        this.setState({paused: true, nowAndStopDiff: this.state.timeToStop.getSeconds() - Date.now().getSeconds()});
+        this.setState({paused: true, nowAndStopDiff: this.state.timeToStop.getSeconds() - new Date().getSeconds()});
     }
 
     // when use clicks the play button
     playWorkout()
     {
         // use the previously recorded difference to update the new timetostop
-        let tempTime = Date.now();
-        tempTime.setSeconds(tempTime.getSeconds() + nowAndStopDiff);
+        let tempTime = new Date();
+        tempTime.setSeconds(tempTime.getSeconds() + this.state.nowAndStopDiff);
         this.setState({paused: false, timeToStop: tempTime, nowAndStopDiff: 0});
 
         // timeToStop needs to be updated to the
@@ -105,21 +109,18 @@ class WorkoutVideo extends React.Component {
 */
 
     render() {
-        return <div>
-            <Header />
-            <div className={styles.workoutvidContainer}>
-                <div className={styles.videoStatusBar}>
-                    <h1>Set {this.state.curSet}</h1>
-                </div>
-                <CurrentExerciseCard exerciseID={exerciseIDArr[this.state.curIndex]} time={timeArr[this.state.curIndex]}></CurrentExerciseCard>
-                <div className={styles.videoControls}>
-                    <MdSkipPrevious className={styles.videoControlsIcon}/>
-                    {this.state.paused && <MdPlayArrow onClick={this.playWorkout} className={styles.videoControlsIcon}/>}
-                    {!this.state.paused && <MdPause onClick={this.pauseWorkout} className={styles.videoControlsIcon}/>}
-                    <MdSkipNext onClick={this.skipNextExercise} className={styles.videoControlsIcon}/>
-                </div>
+        return <div className={styles.workoutvidContainer}>
+            <div className={styles.videoStatusBar}>
+                <h1>Set {this.state.curSet}</h1>
             </div>
-        </div>
+            <CurrentExerciseCard exerciseID={this.state.exerciseIDArr[this.state.curIndex]} time={this.state.timeArr[this.state.curIndex]}></CurrentExerciseCard>
+            <div className={styles.videoControls}>
+                <MdSkipPrevious className={styles.videoControlsIcon}/>
+                {this.state.paused && <MdPlayArrow onClick={this.playWorkout} className={styles.videoControlsIcon}/>}
+                {!this.state.paused && <MdPause onClick={this.pauseWorkout} className={styles.videoControlsIcon}/>}
+                <MdSkipNext onClick={this.skipNextExercise} className={styles.videoControlsIcon}/>
+            </div>
+        </div>;
     }
 }
 
@@ -138,4 +139,17 @@ class CurrentExerciseCard extends React.Component {
     }
 }
 
-export default WorkoutVideo
+function Workout({ workoutid }) {
+    return <div className={styles.pageWrapper}>
+        <Header />
+        <div className={styles.pageContent}>
+            <WorkoutVideo workoutId={workoutid} />
+        </div>
+    </div>;
+}
+
+Workout.getInitialProps = async ({ query }) => {
+    const { workoutid } = query;
+    return { workoutid };
+};
+export default Workout
