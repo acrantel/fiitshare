f84f2a7a6ba1da1dba4465eb56c28f2af4eccc99
Serena Li <serena.li.usa@gmail.com>
2020-07-17 21:38:25 -0700

    Style logo on loading page

--- components/loading.js
+++ components/loading.js
@@ -4,7 +4,8 @@ import styles from './loading.module.css';
 export default class ErrorPage extends React.Component {
     render() {
         return <div className={styles.wrapper}>
-            <img className={styles.logo} src="/images/logo.png" alt="Loading" />
+            <img className={styles.wordLogo} src="/images/wordlogo.png" alt="Loading" />
+            <img className={styles.iconLogo} src="/images/iconlogo.png" alt="Loading" />
         </div>;
     }
 }
