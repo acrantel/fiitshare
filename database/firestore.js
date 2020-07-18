0c2ef88a37a675b0f117344cf5265db4fcfb323f
SheepTester <ussa.say@gmail.com>
2020-07-18 00:22:55 -0700

    Move API keys into a gitignored file (again); DESTROY random, once and for all

--- database/firestore.js
+++ database/firestore.js
@@ -2,15 +2,7 @@ import firebase from 'firebase/app';
 import 'firebase/auth';
 import 'firebase/firestore';
 
-const firebaseConfig = {
-  apiKey: "AIzaSyA1OEYkGbExSFQWVF3U2bX4xm3K0UvHgQ4",
-  authDomain: "bay-area-hacks.firebaseapp.com",
-  databaseURL: "https://bay-area-hacks.firebaseio.com",
-  projectId: "bay-area-hacks",
-  storageBucket: "bay-area-hacks.appspot.com",
-  messagingSenderId: "694610619201",
-  appId: "1:694610619201:web:029eacc8f995fd70fb8fc0"
-};
+import firebaseConfig from '../firebaseConfig.json';
 
 if (!firebase.apps.length) {
   firebase.initializeApp(firebaseConfig);
